8a6c6c762e1993c8733be09d475b88055a64f21c
rakibahmeddev <rakibahmed.dev@gmail.com>
2025-08-05 22:57:53 +0600

    featch is working and phones are displaying now

--- src/components/Phones.jsx
+++ src/components/Phones.jsx
@@ -2,10 +2,36 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const Phones = () => {
-    const phones = useLoaderData();
+  const phones = useLoaderData();
   return (
-    <div>
-      <h2>All Phones {phones.length}</h2>
+    <div className="px-4">
+      <h2 className="text-2xl font-bold my-6">All Phones ({phones.length})</h2>
+
+      {/* Responsive Grid */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {phones.map((phone) => (
+          <div
+            key={phone.id}
+            className="text-center shadow-lg p-4 rounded-lg bg-gray-100 hover:bg-gray-200 transition duration-300"
+          >
+            <img
+              src={phone.image}
+              alt={phone.name}
+              className=" mx-auto rounded-md"
+            />
+            <h3 className="text-xl font-bold mt-4">{phone.name}</h3>
+            <p className="text-gray-600 mb-2">${phone.price}</p>
+            <div className="flex gap-x-2  justify-center">
+              <button className="bg-amber-300 px-6 py-1 rounded-sm cursor-pointer">
+                View Detail
+              </button>
+              <button className="border-2 border-amber-300 bg-white px-6 py-1 rounded-sm cursor-pointer">
+                Buy Now
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
